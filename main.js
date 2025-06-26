b2cda2a93637108bb19c1443618c40a47f01f555
Chidubem <dubemnwabuisi@gmail.com>
2025-06-26 05:57:13 +0100

    this is the initial and first commit

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const { app, BrowserWindow, Notification, ipcMain } = require("electron");
+const Store = require('electron-store');
+const path = require('path');
 
 let mainWindow;
+let breakWindow; // for displaying the images used during the stretch break
 let eyeExerciseTimer;
+let stretchTimer;
+
+// Interval
 let eyeExerciseInterval = 2 * 60 * 1000; // 30 minutes in milliseconds
+const DEFAULT_STRETCH_INTERVAL = 60 * 60 * 1000; // 1 hour in milliseconds
+const DEFAULT_EYE_EXERCISE_INTERVAL = 30 * 60 * 1000; // 30 minutes in milliseconds
+
+// store user preferences. By default electron do not persist data. We use electron-store
+
+const store = new Store ({
+    defaults: {
+        eyeExerciseInterval: DEFAULT_EYE_EXERCISE_INTERVAL,
+        stretchInterval: DEFAULT_STRETCH_INTERVAL,
+
+    },
+});
 
 const createWindow = () => {
 	mainWindow = new BrowserWindow({
@@ -12,7 +30,6 @@ const createWindow = () => {
 		webPreferences: {
 			nodeIntegration: true,
 			contextIsolation: false,
-			enableRemoteModule: true,
 		},
 	});
 
@@ -24,6 +41,11 @@ const createWindow = () => {
 	mainWindow.on("closed", () => {
 		mainWindow = null;
 		clearTimeout(eyeExerciseTimer);
+        clearTimeout(stretchTimer);
+
+        if (breakWindow) {
+            breakWindow.close();
+        }
 	});
 };
 
